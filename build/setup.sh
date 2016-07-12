558c2d87bd8affcfe047e4d6e2cf15ca85b13fcf
bernie macias <berniem@zillow.com>
2016-07-11 20:42:29 -0700

    update setup and ignore

--- build/setup.sh
+++ build/setup.sh
@@ -4,10 +4,10 @@
 BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
 FILENAME_BASE=${BASEDIR##*/}
 
-echo $(pip install --prefix=${BASEDIR}/bin  splunk-sdk )
+echo $(pip install --prefix=${BASEDIR}/bin  'splunk-sdk==1.6.0' )
 echo $(pip install --prefix=${BASEDIR}/bin  requests )
-echo $(mv "${BASEDIR}/bin/lib/python2.7/site-packages/splunklib" "${BASEDIR}/bin")
-echo $(mv "${BASEDIR}/bin/lib/python2.7/site-packages/requests" "${BASEDIR}/bin")
-echo $(rm -rf "${BASEDIR}/bin/lib")
+$(mv "${BASEDIR}/bin/lib/python2.7/site-packages/splunklib" "${BASEDIR}/bin")
+$(mv "${BASEDIR}/bin/lib/python2.7/site-packages/requests" "${BASEDIR}/bin")
+$(rm -rf "${BASEDIR}/bin/lib")
 
 
